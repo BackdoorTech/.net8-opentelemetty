459ae825215d2f78f0c51c7248a07280ebba2f4f
Peter Maquiran <peter.maquiran@equilibrium.local>
2025-01-04 20:55:02 +0100

    add static port

--- obj/Debug/net8.0/DoTAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DoTAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoTAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5804b9af6f00ca9aa2d5183f62650290c48942d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589660fe327ca2aa195e1963e7ad354f97a469cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoTAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoTAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
